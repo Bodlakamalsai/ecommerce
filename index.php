12505056a07551f9b09a5efa7a1b2c4c8af0a718
kamal sai <51473051+Bodlakamalsai@users.noreply.github.com>
2021-04-10 09:38:55 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<style>
+.btn-group button {
+  background-color: #4CAF50; /* Green background */
+  border: 1px solid green; /* Green border */
+  color: white; /* White text */
+  padding: 10px 24px; /* Some padding */
+  cursor: pointer; /* Pointer/hand icon */
+  float: left; /* Float the buttons side by side */
+}
+
+/* Clear floats (clearfix hack) */
+.btn-group:after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.btn-group button:not(:last-child) {
+  border-right: none; /* Prevent double borders */
+}
+
+/* Add a background color on hover */
+.btn-group button:hover {
+  background-color: #3e8e41;
+}
+</style>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+	      <!-- Main content -->
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-12">
+	        		<?php
+	        			if(isset($_SESSION['error'])){
+	        				echo "
+	        					<div class='alert alert-danger'>
+	        						".$_SESSION['error']."
+	        					</div>
+	        				";
+	        				unset($_SESSION['error']);
+	        			}
+	        		?>
+	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		                <ol class="carousel-indicators">
+		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+		                </ol>
+		                <div class="carousel-inner">
+		                  <div class="item active">
+		                    <img src="images/banner1.png" alt="First slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/banner2.png" alt="Second slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/banner3.png" alt="Third slide">
+		                  </div>
+		                </div>
+		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+		                  <span class="fa fa-angle-left"></span>
+		                </a>
+		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+		                  <span class="fa fa-angle-right"></span>
+		                </a>
+		            </div>
+		            
+	        	</div>
+	        	
+	        </div>
+			<div class="btn-group" style="width:100%">
+  <button style="width:25%" button onclick="document.location='category.php?category=laptops'">laptops</button>
+  <button style="width:25%" button onclick="document.location='category.php?category=desktop-pc'">Desktop</button>
+  <button style="width:25%" button onclick="document.location='category.php?category=tablets'">Tablets</button>
+  <button style="width:25%" button onclick="document.location='category.php?category= '">Smart Phones</button>
+</div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	<?php include 'includes/footer.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
